5e7cb15f0aae610e74e88f028f302bcb56164d11
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-29 14:50:59 +0200

    add header and its content

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div>
+
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </figure>
+            <ul>
+                <li v-for="item in items" :key="item.text"><a :href="item.url">{{ item.text }}</a></li>
+            </ul>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss"></style>
