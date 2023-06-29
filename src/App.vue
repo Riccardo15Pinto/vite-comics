684c00df2da45e1c868c97c39fec9c47fad89378
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-29 17:26:44 +0200

    import AppFooter

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppJumbo from './components/AppJumbo.vue';
 import AppMain from './components/AppMain.vue';
-import AppFooter from './components/AppFooter.Vue';
+import AppFooter from './components/AppFooter.Vue'
 export default {
   components: { AppHeader, AppJumbo, AppMain, AppFooter },
   data() {
