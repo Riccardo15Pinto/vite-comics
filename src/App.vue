91cef03b174c12edf3753192f95d4a2f8bf6cd41
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-29 14:40:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+export default {
+  components: {
+    AppNavbar
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <AppNavbar />
+</template>
+
+<style lang="scss">
+@use '../src/assets/scss/style.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import '@fontsource/fira-sans-condensed/200.css';
+import '@fontsource/fira-sans-condensed/600.css';
+import App from './App.vue'
+
+createApp(App).mount('#app')
