2bcbd9de36e51801cbb5f42b6be0a0d51685aa59
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-30 14:13:49 +0200

    set jumbo section

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -46,8 +46,15 @@ export default {
 </script>
 
 <template>
-    <section class="empty-content">
-        <div>
+    <section class="jumbo-section">
+        <div class="top-jumbo">
+            <h2>Current Series</h2>
+        </div>
+        <div class="bottom-jumbo">
+            <div class="container-card">
+                <ComicCard v-for="comic in comics" :card=comics :thumb="comic" :price="comic" :series="comic"
+                    :key="comic.series" />
+            </div>
         </div>
     </section>
     <section class="info-merch">
@@ -66,14 +73,37 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.empty-content {
-    background-color: #1C1C1C;
+.jumbo-section {
+    .top-jumbo {
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 400px;
 
-    div {
+        h2 {
+            background-color: #0282F9;
+            padding: 10px 20px;
+            display: inline-block;
+            text-transform: uppercase;
+            color: white;
+        }
+    }
+
+    .bottom-jumbo {
+        background-color: #1C1C1C;
         padding: 40px 0;
-        width: 1300px;
-        margin: 0 auto;
         color: white;
+
+        .container-card {
+            margin: 0 auto;
+            width: 1300px;
+            display: flex;
+            flex-wrap: wrap;
+
+            .card {
+                flex-basis: calc(100% / 6);
+            }
+        }
     }
 }
 
