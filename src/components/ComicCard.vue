dbaa1351dfe583c6285155a21b5dbd9816f86953
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-30 14:13:33 +0200

    set template

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -9,11 +9,20 @@ export default {
         card: {
             thumb: 'String',
             price: 'String',
+            series: 'String'
         }
     }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="card">
+        <figure>
+            <img :src="thumb" :alt="series">
+        </figure>
+
+        <h3>{{ series }}</h3>
+    </div>
+</template>
 
 <style lang="scss" scoped></style>
