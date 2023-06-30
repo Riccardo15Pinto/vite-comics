9857ce8f044fd0c012d8a92591d5a64d35c951a3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-30 17:28:23 +0200

    set comiccard

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -5,12 +5,8 @@ export default {
 
         }
     },
-    promps: {
-        card: {
-            thumb: 'String',
-            price: 'String',
-            series: 'String'
-        }
+    props: {
+        comic: 'Object'
     }
 }
 </script>
@@ -18,11 +14,19 @@ export default {
 <template>
     <div class="card">
         <figure>
-            <img :src="thumb" :alt="series">
+            <img :src="comic.thumb" :alt="comic.series">
         </figure>
 
-        <h3>{{ series }}</h3>
+        <h3>{{ comic.series }}</h3>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+    padding: 20px;
+
+    img {
+        height: 300px;
+    }
+}
+</style>
